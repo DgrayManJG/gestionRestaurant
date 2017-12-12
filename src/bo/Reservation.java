58380a9ab046b50589457fa40e7c70472d4361e2
GUEVEL Jimmy <jimmy.guevel2016@campus-eni.fr>
2017-12-12 16:08:50 +0100

    bo reservation

--- src/bo/Reservation.java
+++ src/bo/Reservation.java
@@ -1,12 +1,15 @@
 package bo;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Reservation {
@@ -16,6 +19,38 @@ public class Reservation {
 	private int id;
 	@Column(name="nom", length=40)
 	private String nom;
+	@Column(name="date")
 	private Date date;
+	@OneToMany
+	private List<Table> table = new ArrayList<Table>();
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public List<Table> getTable() {
+		return table;
+	}
+	public void setTable(List<Table> table) {
+		this.table = table;
+	}
+	
+	@Override
+	public String toString() {
+		return "Reservation [id=" + id + ", nom=" + nom + ", date=" + date + "]";
+	}
 }
