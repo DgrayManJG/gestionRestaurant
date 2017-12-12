812d7bfc0ce11b987670cda3091f47c5e71749c7
= <=>
2017-12-12 16:41:30 +0100

    Correction erreur de Jimmy

--- src/bo/Table.java
+++ src/bo/Table.java
@@ -30,4 +30,65 @@ public class Table {
 	@ManyToOne
 	private Reservation reservation;
 	
+	public Table() {
+		
+	}
+
+	public Table(int id, int number, String decription, Statut statut, Service service, Reservation reservation) {
+		super();
+		this.id = id;
+		this.number = number;
+		this.decription = decription;
+		this.statut = statut;
+		this.service = service;
+		this.reservation = reservation;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getDecription() {
+		return decription;
+	}
+
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+
+	public Statut getStatut() {
+		return statut;
+	}
+
+	public void setStatut(Statut statut) {
+		this.statut = statut;
+	}
+
+	public Service getService() {
+		return service;
+	}
+
+	public void setService(Service service) {
+		this.service = service;
+	}
+
+	public Reservation getReservation() {
+		return reservation;
+	}
+
+	public void setReservation(Reservation reservation) {
+		this.reservation = reservation;
+	}	
 }
