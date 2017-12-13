841b9fe0e603d537d6a78a270c2d3df02e114a65
GUEVEL Jimmy <jimmy.guevel2016@campus-eni.fr>
2017-12-13 09:17:53 +0100

    models update

--- src/bo/Commande.java
+++ src/bo/Commande.java
@@ -3,11 +3,36 @@ package bo;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 public class Commande {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
+	@ManyToMany
+	private Plat plat;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Plat getPlat() {
+		return plat;
+	}
+
+	public void setPlat(Plat plat) {
+		this.plat = plat;
+	}
+
+	@Override
+	public String toString() {
+		return "Commande [id=" + id + ", plat=" + plat + "]";
+	}
 	
 }
--- src/bo/Plat.java
+++ src/bo/Plat.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 public class Plat {
@@ -20,5 +21,54 @@ public class Plat {
 	
 	@OneToMany
 	private Type type;
+	
+	@ManyToMany
+	private Commande commande;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public Double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(Double prix) {
+		this.prix = prix;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public Commande getCommande() {
+		return commande;
+	}
+
+	public void setCommande(Commande commande) {
+		this.commande = commande;
+	}
+
+	@Override
+	public String toString() {
+		return "Plat [id=" + id + ", libelle=" + libelle + ", prix=" + prix + ", type=" + type + ", commande="
+				+ commande + "]";
+	}
 
 }
